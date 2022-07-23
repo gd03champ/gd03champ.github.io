413f66a96da81f93d6ab2075627d117ffbd0d136
GD <63779654+gd03champ@users.noreply.github.com>
2022-07-23 23:29:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,519 @@
+let homeContent = `<div class="content-wrapper">
+<section class="home ">
+    <div class="greetings content-card">
+        <div class="greetings-title">
+            <h1 class="t1"> Hello! </h1>
+            <h1 class="t2"> My Name is <span>GD</span> </h1>
+            <h1 class="t3"> I am a Programmer 🚀</h1>
+        </div>
+
+    </div>
+
+</section>`;
+
+let aboutContent = `<section class="about-wrapper">
+<div class="about-section about-content-card">
+  <div class="about-content">
+    <div class="about-title">
+      <h1>About</h1>
+    </div>
+    <div class="about-text">
+      <p>
+        My name is GD.<br />
+        In 2020 [17], I discovered my passion towards programming and technology which derived from gaming 🎮<br />
+        I wanted to learn about what I was really interested in and what I
+        enjoyed doing.<br />
+        and so I began putting time and effort into learning whatever excites me about in dev community and started writing some cool scripts<br />
+        Later I joined a regular decent college <br />
+        But I wanted to do more. I never wanted to settle for average.<br />
+        <br />
+       
+        <p>Typing...</p>
+        <!--I began with <i class="fab fa-html5"></i> and
+        <i class="fab fa-css3-alt"></i> . I created a few landing pages. As
+        I have already worked in PhotoShop, PrmierPro, AfterEffects I
+        independently created mock-ups and turned them into code. <br />When
+        I felt ready to move on, I turned to
+        <i class="fab fa-js"></i> .<br />
+        <br />Then I realized that I needed someone to help me find the
+        right direction. <br />ComTrade had a Front End Development course,
+        with a strong focus on JS. I enrolled in it.<br />
+        In September 2019, I graduated with a grade 10.<br />
+        As I was studying, I also worked on JS algorithms on the
+        <i class="fab fa-free-code-camp"></i> and I created pages and small
+        application which are not complete as they have been constructed
+        with a specific idea and testing of new options. When I felt
+        comfortable working with JS i went on to learn-->
+        
+        <br />
+        As i went deeper into programming i wanted to do more than just programming so i started learning hacking 
+      </p>
+    </div>
+  </div>
+</div>
+</section>`;
+
+let workContent = `
+
+        <div class="work-content work-content-card">
+
+            <div class="work-gallery">
+
+            <div class="skills-title">
+            <h1>My Works</h1>
+            </div><br>
+
+                <div class="work-project-card card-6">
+                    <h1 class="work-project-card-title tooltip ">
+                    BIT Assisstant  <span class="tooltiptext">
+                    Project idea: 
+                    <br>-NLU Chatbot 
+                    <br>-Tokenized Webscrapping 
+                    
+                    </span>
+                    </h1>
+                    <a href="http://10.30.10.18/" target="_blank">
+                    <img class="workPic tooltip" src="assets/bit-assistant.png"> </a>
+                    <a href="https://github.com/gd03champ/BIT-Assistant/blob/main/README.md">  
+                    <p class="button-project btn-github ">Go to GITHUB </p></a>
+                </div>
+
+                <div class="work-project-card card-5">
+                    <h1 class="work-project-card-title tooltip">
+                        YouTube Automation <span class="tooltiptext">
+                        Project idea: 
+                        <br>-Instagram Scrapping
+                        <br>-Moviepy
+                        <br>-YouTube V3 API
+                        
+                        </span>
+                    </h1>
+                    <a href="https://www.youtube.com/channel/UCg3gxUJ5DbcYrHYCsrC0Y" target="_blank">
+                    <img class="workPic" src="assets/ytube-aut.jpg"></a>
+                    <a href="https://github.com/gd03champ/YouTube-Automation" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-4">
+                        <h1 class="work-project-card-title tooltip">
+                        It's Hero Time<span class="tooltiptext">
+                        Project idea:
+                        <br>- BEN 10 
+                        <br>-HTML, CSS & JS
+                        <br>-Interactive website
+                        
+                        </span>
+                    </h1>
+                        <a href="https://gd03champ.github.io/itsherotime/" target="_blank">
+                    <img class="workPic" src="assets/itsherotime.png"> </a>
+                    <a href="https://github.com/gd03champ/itsherotime" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-3">
+                    <h1 class="work-project-card-title tooltip">
+                        Emote based Ads <span class="tooltiptext">
+                        Project idea: 
+                        <br>-Emotion Recognition 
+                        <br>-Custom Advertisement
+                        <br>-PHP, Python, ML 
+                        
+                        </span>
+                    </h1>
+                    <a target="_blank">
+                    <img class="workPic" src="assets/face.png"> </a>
+                    <a href="https://github.com/gd03champ/Emotion-Based-Ad-Recommendation" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-2">
+                <h1 class="work-project-card-title tooltip">
+                    Web RTC VC <span class="tooltiptext">
+                    Project idea: 
+                    <br>-Web RTC Video Call
+                    <br>-Private Room Sessions
+                    <br>-Flask UI
+                    
+                    </span>
+                </h1>
+                <a href="https://clubchat-gd.herokuapp.com/" target="_blank">
+                <img class="workPic" src="assets/webrtc.png"> </a>
+                <a href="https://github.com/gd03champ/WebRTC-Video-Call" target="_blank">  
+                <p class="button-project btn-github ">Go to GITHUB</p>
+                </a>
+            </div>
+
+
+                <div class="work-project-card card-2">
+                    <h1 class="work-project-card-title tooltip">
+                        Questionnaire Bot <span class="tooltiptext">
+                        School Project: 
+                        <br>-Quetion & Answer Gen
+                        <br>-NLU ChatBot
+                        <br>-react.js UI
+                        
+                        </span>
+                    </h1>
+                    <a target="_blank">
+                    <img class="workPic" src="assets/questionnaire.jpeg"> </a>
+                    <a href="https://github.com/gd03champ/Questionnaire-Bot" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-2">
+                <h1 class="work-project-card-title tooltip">
+                    Coming Soon <span class="tooltiptext">
+                    School Project: 
+                    <br>-
+                    <br>-
+                    <br>-
+                    
+                    </span>
+                </h1>
+                <a target="_blank">
+                <img class="workPic" src="assets/tic.jpg"> </a>
+                <a href="https://github.com/gd03champ/" target="_blank">  
+                <p class="button-project btn-github ">Go to GITHUB</p>
+                </a>
+            </div>
+                
+            </div>
+            <!--<p class="mobile">GO MOBILE</p>-->
+        </div>
+      
+  
+`;
+
+let skillsContent = `<div class="skills-section">
+<div class="skills-content skills-content-card">
+    <div class="skills-title">
+        <h1>Skills</h1>
+    </div>
+    <div class="skills-text">
+        <p> Competitive Programming <br> 
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;-> Python  ||||||||||||||||||||<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;-> C  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  ||||||||||||||<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;-> JAVA  &nbsp;&nbsp;&nbsp;  ||||||||||<br></p><br>
+        <p> Linux Administration ||||||||||||||||||</p>
+        <p> Networking ||||||||||||||||</p>
+        <p> Cloud Programming and Deploynment ||||||||||||||||</p>
+        <p> Computer Vision, AI & ML ||||||||||||||||||||</p>
+        <p>Firebase ||||||||||||</p>
+        <p>GIT ||||||||||||||||||||</p>
+        <p> HTML, CSS, JavaScript, React ||||||||||||||||||||</p>
+        <p> PHP ||||||||||||||||</p>
+        <p> MySQL ||||||||||||||||||||</p>
+        
+    </div>
+</div>
+</div>`;
+
+let mobileContent = `<section class="work-wrapper">
+<div class="work-content work-content-card">
+    <div class="work-gallery">
+
+        <div class="work-project-card card-6">
+            <h1 class="work-project-card-title ">
+            Real Estate gold
+            </h1>
+            <a href="https://hopeful-einstein-2a0f32.netlify.com/" target="_blank">
+            <img class="qr-card" src="./QRprojects/realestateQR.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/Real-Estate-gold">  
+            <p class="button-project btn-github ">Go to GITHUB</p></a>
+        </div>
+
+        <div class="work-project-card card-5">
+            <h1 class="work-project-card-title ">
+                Digital Agency
+            </h1>
+            <a href="https://djordjevicn.github.io/Digital-Agency/" target="_blank">
+            <img class="qr-card" src="./QRprojects/digitalagensyQR.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/Digital-Agency" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-4">
+                <h1 class="work-project-card-title ">
+                Mountain Parallax
+            </h1>
+                <a href="https://djordjevicn.github.io/Parallax-practise-Mountin/" target="_blank">
+            <img class="qr-card" src="./QRprojects/paralaxMountinQR.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/Parallax-practise-Mountin" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-3">
+            <h1 class="work-project-card-title ">
+                Shoe Shop
+            </h1>
+            <a href="https://djordjevicn.github.io/tezga-shop-shoes/" target="_blank">
+            <img class="qr-card" src="./QRprojects/tezgaShopQR.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/tezga-shop-shoes" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-2">
+            <h1 class="work-project-card-title ">
+                Burger Online
+            </h1>
+            <a href="https://djordjevicn.github.io/eCommerce-Burgelicious/" target="_blank">
+            <img class="qr-card" src="./QRprojects/burgelicious.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/eCommerce-Burgelicious" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-1">
+            <h1 class="work-project-card-title ">
+                Movie Search
+            </h1>
+            <a href="https://djordjevicn.github.io/Movie-Search-API-/" target="_blank">
+            <img class="qr-card" src="./QRprojects/movieSearchQR.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/Movie-Search-API-" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        
+    </div>
+    <p class="mobile">GO MOBILE</p>
+</div>
+
+</section>`;
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const work = document.querySelector("#work");
+const skills = document.querySelector("#skills");
+let contentBox = document.querySelector(".content-wrapper");
+let phoneContact = document.querySelector(".letsTalk");
+let enter = document.querySelector(".preloader-wrapper");
+
+document.addEventListener("DOMContentLoaded", () => {
+  let enterSequence = new TimelineMax({});
+
+  contentBox.innerHTML = "";
+  contentBox.innerHTML = homeContent;
+  enterSequence
+    .from(".letsTalk", 1, {
+      x: -300
+    })
+    .from("#home", 0.1, {
+      opacity: 0
+    })
+    .from("#home", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from("#about", 0.1, {
+      opacity: 0
+    })
+    .from("#about", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from("#work", 0.1, {
+      opacity: 0
+    })
+    .from("#work", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from("#skills", 0.1, {
+      opacity: 0
+    })
+    .from("#skills", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from(".home", 1, {
+      opacity: 0,
+      x: -300
+    })
+    .from(".t1", 0.1, {
+      opacity: 0
+    })
+    .from(".t2", 0.1, {
+      opacity: 0
+    })
+    .from(".t3", 0.1, {
+      opacity: 0
+    })
+    .from(".interface-settings li", 0.9, {
+      x: 550
+    });
+
+  home.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = homeContent;
+    let openHome = new TimelineMax({});
+    openHome
+      .from(".home", 1, {
+        opacity: 0,
+        x: -300
+      })
+      .from(".t1", 0.1, {
+        opacity: 0
+      })
+      .from(".t2", 0.1, {
+        opacity: 0
+      })
+      .from(".t3", 0.1, {
+        opacity: 0
+      });
+  });
+  about.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = aboutContent;
+    let openAbout = new TimelineMax({});
+    openAbout
+      .from(".about-section", 1, {
+        opacity: 0,
+        x: 300
+      })
+      .from(
+        ".about-title h1",
+        0.1,
+        {
+          opacity: 0
+        },
+        "ww"
+      )
+      .from(".about-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".about-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(".about-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".about-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(".about-text p", 0.2, {
+        opacity: 0
+      });
+  });
+  work.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = workContent;
+    let openWork = new TimelineMax({});
+
+    openWork
+      .from(".work-content-card", 1, {
+        opacity: 0,
+        x: -300
+      })
+      .from(
+        ".card-5",
+        0.1,
+        {
+          opacity: 0
+        },
+        "op"
+      )
+      .from(
+        ".card-2",
+        0.1,
+        {
+          opacity: 0
+        },
+        "op"
+      )
+      .from(
+        ".card-3",
+        0.1,
+        {
+          opacity: 0
+        },
+        "op"
+      )
+      .from(
+        ".card-6",
+        0.1,
+        {
+          opacity: 0
+        },
+        "oz"
+      )
+      .from(
+        ".card-1",
+        0.1,
+        {
+          opacity: 0
+        },
+        "oz"
+      )
+      .from(
+        ".card-4",
+        0.1,
+        {
+          opacity: 0
+        },
+        "oz"
+      );
+
+    let goMobile = document.querySelector(".mobile");
+    goMobile.addEventListener("click", () => {
+      contentBox.innerHTML = "";
+      contentBox.innerHTML = mobileContent;
+    });
+  });
+
+  skills.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = skillsContent;
+    let openSkills = new TimelineMax({});
+    openSkills
+      .from(".skills-content", 0.6, {
+        opacity: 0,
+        x: 300
+      })
+      .from(
+        ".skills-title h1",
+        0.1,
+        {
+          opacity: 0
+        },
+        "ww"
+      )
+      .from(".skills-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".skills-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(".skills-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".skills-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(
+        ".skills-text",
+        0.2,
+        {
+          opacity: 0
+        },
+        "ww",
+        1
+      );
+  });
+  let openContact = new TimelineMax({ paused: true, reversed: true });
+  phoneContact.addEventListener("click", () => {
+    phoneContact.classList.toggle("opened");
+
+    openContact.reversed() ? openContact.play() : openContact.reverse();
+
+    openContact.to(".interface-settings li", 0.3, {
+      x: 550
+    });
+  });
+});
